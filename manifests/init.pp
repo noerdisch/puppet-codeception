28bde1d159136eda10e3831c6dd6f2843a8fe0a3
Markus Günther <markus.guenther@noerdisch.de>
2017-10-17 11:47:05 +0200

    [FEATURE] Add manifest file and the script templates

---
+++ manifests/init.pp
@@ -0,0 +1,60 @@
+class codeception {
+  exec { 'chrome_install':
+    command => 'sudo apt-get install google-chrome-stable',
+  }
+
+  exec { 'chromedriver_install':
+    command => 'wget https://chromedriver.storage.googleapis.com/2.33/chromedriver_linux64.zip -P ~/ && unzip ~/chromedriver_linux64.zip -d ~/ && rm ~/chromedriver_linux64.zip && sudo mv -f ~/chromedriver /usr/local/bin/chromedriver',
+  }
+  ->
+  file { "/usr/local/bin/chromedriver":
+    ensure => present,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+  }
+
+  exec { 'codeception_install':
+    command => 'wget http://codeception.com/codecept.phar && sudo mv ./codecept.phar /usr/local/bin/codecept',
+    path    => '/usr/bin:/usr/sbin',
+  }
+  ->
+  file { "/usr/local/bin/codecept":
+    ensure => present,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+  }
+
+  file { "/usr/local/bin/start-chrome":
+    ensure          => file,
+    content         => template("noerdception/start-chrome.sh.erb"),
+    owner            => 'root',
+    group            => 'root',
+    mode             => '0755'
+  }
+
+  file { "/usr/local/bin/start-typo3-listener":
+    ensure          => file,
+    content         => template("noerdception/start-typo3-listener.sh.erb"),
+    owner            => 'root',
+    group            => 'root',
+    mode             => '0755'
+  }
+
+  file { "/usr/local/bin/stop-chrome":
+    ensure          => file,
+    content         => template("noerdception/stop-chrome.sh.erb"),
+    owner            => 'root',
+    group            => 'root',
+    mode             => '0755'
+  }
+
+  file { "/usr/local/bin/stop-typo3-listener":
+    ensure          => file,
+    content         => template("noerdception/stop-typo3-listener.sh.erb"),
+    owner            => 'root',
+    group            => 'root',
+    mode             => '0755'
+  }
+}
