48821dc08d80ebf2eadb9615790cd430904fdc0e
Markus Günther <markus.guenther@noerdisch.de>
2017-11-06 20:26:34 +0100

    [BUGFIX] Use deb file to install chrome

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,16 +12,12 @@
 #   include codeception
 class codeception {
   if $::osfamily == 'Debian' {
-    exec { 'key signing chrome':
-      command => 'wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -',
+    exec { 'download chrome source':
+      command => 'wget -N https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/',
     }
 
-    exec { 'Add chrome to sourcelist':
-      command => 'sudo sh -c \'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list\'',
-    }
-
-    exec { 'chrome_install':
-      command => 'sudo apt-get update && sudo apt-get install google-chrome-stable',
+    exec { 'install chrome':
+      command => 'sudo dpkg -i --force-depends ~/google-chrome-stable_current_amd64.deb && sudo apt-get -f install -y',
     }
   }
 
