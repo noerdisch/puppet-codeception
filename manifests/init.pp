aa394cc7bdbe71615ad7364f9837506ff2db83d0
Markus Günther <markus.guenther@noerdisch.de>
2017-11-06 20:07:42 +0100

    [BUGFIX] Escape single quotes in command

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,7 +17,7 @@ class codeception {
     }
 
     exec { 'Add chrome to sourcelist':
-      command => 'sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'',
+      command => 'sudo sh -c \'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list\'',
     }
 
     exec { 'chrome_install':
