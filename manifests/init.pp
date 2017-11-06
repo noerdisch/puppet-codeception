104647c3d37eec612880fe5b96bae344bcd2b468
Markus Günther <markus.guenther@noerdisch.de>
2017-11-06 19:57:57 +0100

    [BUGFIX] Add key sighning and extend sources list

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,8 +12,16 @@
 #   include codeception
 class codeception {
   if $::osfamily == 'Debian' {
+    exec { 'key signing chrome':
+      command => 'wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -',
+    }
+
+    exec { 'Add chrome to sourcelist':
+      command => 'sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'',
+    }
+
     exec { 'chrome_install':
-      command => 'sudo apt-get install google-chrome-stable',
+      command => 'sudo apt-get update && sudo apt-get install google-chrome-stable',
     }
   }
 
